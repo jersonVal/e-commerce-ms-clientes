30633656ce746f5924f100dc8f06f8ff0c2e5f7e
jersonval96 <jersonDevelopment@gmail.com>
2021-09-13 11:41:56 -0500

    adding new model and datasource

---
+++ src/models/cliente.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Cliente extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  documento: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  celular: number;
+
+  @property({
+    type: 'number',
+    default: 1,
+  })
+  estado?: number;
+
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
